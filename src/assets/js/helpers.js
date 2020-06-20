c17436ae01d3d1a3dc601f9e89b8ec86c500fa40
mhdrahil <mhd.rahil@gmail.com>
2020-06-20 17:29:25 +0530

    First commit

--- src/assets/js/helpers.js
+++ src/assets/js/helpers.js
@@ -116,7 +116,7 @@ export default {
                 }, 
                 {
                     username: "ml0jh0qMKZKd9P_9C0UIBY2G0nSQMCFBUXGlk6IXDJf8G2uiCymg9WwbEJTMwVeiAAAAAF2__hNSaW5vbGVl", 
-                    credential: "4dd454a6-feee-11e9-b185-6adcafebbb45",
+                    credential: "81b2acd8-9a74-11ea-86d8-0242ac150002",
                     urls: [
                         "turn:eu-turn4.xirsys.com:80?transport=udp",                         
                         "turn:eu-turn4.xirsys.com:3478?transport=tcp"
